1e087ef6fb70fe5e11452bfe26f5185f7bdba246
Daniele Orlando <daniele.orlando@gmail.com>
2016-10-26 11:57:11 +0200

    Initial commit

---
+++ app/src/main/java/me/danieleorlando/gradienttransition/MainActivity.java
@@ -0,0 +1,38 @@
+package me.danieleorlando.gradienttransition;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
+    }
+}
